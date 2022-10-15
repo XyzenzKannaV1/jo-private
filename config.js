a37f60390efa8bdba52d0bbcf4bdb122dda6ea3f
Kanna <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-10-15 20:04:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE DANIXD'
+global.ownername = 'Kanna Private'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
+global.owner = ['6283866366437','6283866366437','6283866366437']
+global.premium = ['6283866366437']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
